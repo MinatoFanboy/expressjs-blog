2dbd5a4996b80e98140e06e19666387e5cd742cd
MinatoFanboy <thanh31299@gmail.com>
2022-06-12 20:15:52 +0700

    Routes and Controller

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Http Logger
@@ -12,14 +14,15 @@ app.use(morgan('combined'))
 
 // Template Engine
 app.set('view engine', 'hbs');
+
 app.engine('hbs', handlebars.engine({
   extname: '.hbs'
 }));
+
 app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
+// Routes init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
