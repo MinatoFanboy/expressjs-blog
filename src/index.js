9c0667a8cc5d7deab5240c492b2f481829498a03
MinatoFanboy <thanh31299@gmail.com>
2022-06-13 08:02:47 +0700

    READ from DB

--- src/index.js
+++ src/index.js
@@ -27,11 +27,11 @@ app.engine(
     }),
 );
 
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
